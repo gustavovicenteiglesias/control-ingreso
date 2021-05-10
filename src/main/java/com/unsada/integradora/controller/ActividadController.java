963617e7f11884d1c0324d2d201ab613ad239d06
Thomas Diaz <thomydiaz00@gmail.com>
2021-05-10 15:07:09 -0300

    Set aula pasando aula cohorte y horario

--- src/main/java/com/unsada/integradora/controller/ActividadController.java
+++ src/main/java/com/unsada/integradora/controller/ActividadController.java
@@ -17,12 +17,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.unsada.integradora.model.Actividad;
+import com.unsada.integradora.model.Cohorte;
+import com.unsada.integradora.model.CohorteHorario;
+import com.unsada.integradora.model.EntidadAula;
+import com.unsada.integradora.model.Horario;
 import com.unsada.integradora.model.Propuesta;
+import com.unsada.integradora.model.SesionPresencial;
 import com.unsada.integradora.service.ActividadServiceApi;
+import com.unsada.integradora.service.CohorteHorarioServiceApi;
+import com.unsada.integradora.service.CohorteServiceApi;
+import com.unsada.integradora.service.EntidadAulaServiceApi;
+import com.unsada.integradora.service.HorarioServiceApi;
 import com.unsada.integradora.service.PropuestaServiceApi;
+import com.unsada.integradora.service.SesionPresencialServiceApi;
 
 @RestController
 @RequestMapping(value = "/api/actividad")
@@ -33,6 +44,14 @@ public class ActividadController {
 	@Autowired
 	@Qualifier("propuestaServiceApi")
 	PropuestaServiceApi propuestaServiceApi ;
+	@Autowired CohorteServiceApi cohorteServiceApi;
+	@Autowired HorarioServiceApi horarioServiceApi;
+	@Autowired EntidadAulaServiceApi aulaServiceApi;
+	@Autowired CohorteHorarioServiceApi cohorteHorarioServiceApi;
+	@Autowired SesionPresencialServiceApi sesionPresencialServiceApi;
+
+
+
 
 	@GetMapping(value = "/all")
 	public Map<String, Object> listclase() {
@@ -84,23 +103,44 @@ public class ActividadController {
 
 	@PostMapping(value = "/create")
 	public ResponseEntity<String> create(@RequestBody Actividad data) {
-
 		try {
 			actividadServiceApi.save(data);
 			return new ResponseEntity<>("Save successful ", HttpStatus.OK);
 		} catch (Exception e) {
+			return new ResponseEntity<>("" + e, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 
+	}
+	@GetMapping(value ="/cohortes/{idActividad}")
+	public Map<String, Object> listCohortes(){
+		HashMap<String, Object> response = new HashMap<String, Object>();
+		return response;
+
+
+	}
+	@PostMapping(value="/asignar-aula-por-cohorte-horario/{idAula}/{idCohorte}/{idHorario}")
+	public ResponseEntity<String> asignarAula( @PathVariable("idAula") int idAula, @PathVariable("idCohorte") int idCohorte, @PathVariable("idHorario") int idHorario ) {
+		Optional<Horario> horario = horarioServiceApi.findById(idHorario);
+		Optional<EntidadAula> aula = aulaServiceApi.findById(idAula);
+		Optional<Cohorte> cohorte = cohorteServiceApi.findById(idCohorte);
+		try {
+			Optional<CohorteHorario> cohorteHorario = cohorteHorarioServiceApi.findByCohorteAndHorario(cohorte.get(), horario.get());
+			List<SesionPresencial> sesiones = sesionPresencialServiceApi.findByCohorteHorario(cohorteHorario.get());
+			for (SesionPresencial sesionPresencial : sesiones) {
+				sesionPresencial.setEntidadAula(aula.get());;
+				sesionPresencialServiceApi.save(sesionPresencial);
+			}
+			
+			return new ResponseEntity<>("Save successful ", HttpStatus.OK);
+		} catch (Exception e) {
 			return new ResponseEntity<>("" + e, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 
 	}
 
 	@PutMapping(value = "/update/{id}")
-
 	public Map<String, Object> update(@PathVariable("id") Integer id, @RequestBody Actividad data) {
-
 		HashMap<String, Object> response = new HashMap<String, Object>();
-
 		try {
 			data.setIdActividad(id);
 			actividadServiceApi.save(data);
--- src/main/java/com/unsada/integradora/service/SesionPresencialServiceApi.java
+++ src/main/java/com/unsada/integradora/service/SesionPresencialServiceApi.java
@@ -2,8 +2,13 @@ package com.unsada.integradora.service;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
+import com.unsada.integradora.model.CohorteHorario;
 import com.unsada.integradora.model.SesionPresencial;
 
 public interface SesionPresencialServiceApi extends CrudRepository<SesionPresencial, Integer>{
+
+  List<SesionPresencial> findByCohorteHorario(CohorteHorario cohorteHorario);
 
 }
