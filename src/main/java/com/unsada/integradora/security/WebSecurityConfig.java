ee4dff733131ddb949a8a6003ce57818daa9f496
Gustavo <gviglesias@alumnos.unsada.edu.ar>
2021-07-30 12:34:09 -0300

    jwt3

--- src/main/java/com/unsada/integradora/security/WebSecurityConfig.java
+++ src/main/java/com/unsada/integradora/security/WebSecurityConfig.java
@@ -57,8 +57,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable()
 			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-			.authorizeRequests().antMatchers("api/edificio/all",
-					"api/solicitud/find/uuid/**","api/solicitud/update/**","api/login/**","/api/auth/**").permitAll()
+			.authorizeRequests().antMatchers("/api/edificio/all",
+					"/api/solicitud/find/uuid/**","/api/solicitud/update/**","/api/login/**","/api/auth/**").permitAll()
 			.antMatchers("/api/test/**").permitAll()
 			.anyRequest().authenticated();
 
